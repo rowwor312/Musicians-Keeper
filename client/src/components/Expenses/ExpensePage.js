aa68ea38881073172be69275177a90f30b80cc66
Joe P <joe.peterson2729@gmail.com>
2019-09-25 21:09:04 -0500

    adding categories to the accordian

--- client/src/components/Expenses/ExpensePage.js
+++ client/src/components/Expenses/ExpensePage.js
@@ -54,7 +54,7 @@ export default function ControlledExpansionPanels() {
                     id="panel1bh-header"
                 >
                     <Typography className={classes.heading}>Advertising</Typography>
-                    <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
+                    <Typography className={classes.secondaryHeading}>i.e.(Social media ads, Posters, Flyers, PR, Business Cards, Stage Banner </Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                     <Typography>
@@ -85,8 +85,8 @@ export default function ControlledExpansionPanels() {
                     aria-controls="panel2bh-content"
                     id="panel2bh-header"
                 >
-                    <Typography className={classes.heading}>Users</Typography>
-                    <Typography className={classes.secondaryHeading}>
+                    <Typography className={classes.heading}>Contract Labor</Typography>
+                    <Typography className={classes.secondaryHeading}> i.e. (Pay to band members, Session Artist, Production, Studio time. Over 600 will require 1099 filing)
 
 
                     </Typography>
@@ -122,15 +122,31 @@ export default function ControlledExpansionPanels() {
                     aria-controls="panel3bh-content"
                     id="panel3bh-header"
                 >
-                    <Typography className={classes.heading}>Advanced settings</Typography>
-                    <Typography className={classes.secondaryHeading}>
-                        Filtering has been entirely disabled for whole web server
+                    <Typography className={classes.heading}>Equipment</Typography>
+                    <Typography className={classes.secondaryHeading}>i.e.(Amps, Computer, Guitars, Drums, Keyboard, Recording Gear,Speakers note:if over 1,000 for item may depreciate)
+                        
           </Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                     <Typography>
-                        Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros,
-                        vitae egestas augue. Duis vel est augue.
+                    <form>
+                            <label>
+                                Date:
+    <input type="text" name="Date" />
+
+                                Purchased Location
+    <input type="text" name="Purchased At" />
+
+                                Amount Spent
+    <input type="text" name="Purchased At" />
+
+
+
+                            </label>
+
+
+                            <input type="submit" value="Submit" />
+                        </form>
           </Typography>
                 </ExpansionPanelDetails>
             </ExpansionPanel>
