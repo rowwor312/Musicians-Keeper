608771c04110d6cf7b0c6d269e68507b888f529e
Joe P <joe.peterson2729@gmail.com>
2019-09-27 08:09:30 -0500

    deleted commented out code in expense file

--- client/src/components/Expenses/ExpensePage.js
+++ client/src/components/Expenses/ExpensePage.js
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Button from '@material-ui/core/Button';
-
-// function App() {
-//   return (
-//     <Button variant="contained" color="primary">
-//       Hello World
-//     </Button>
-//   );
-// }
-
-// ReactDOM.render(<App />, document.querySelector('#app'));
-
 
 
 import React from 'react';
